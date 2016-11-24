f3a6a0c663eb385d5bf0aa42be8b40635b60aff0
deanfoulds <deanfoulds@gmail.com>
2016-11-24 16:02:38 +0000

    CTA added and link to index page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     root to: "users#index"
   end
   root to: 'visitors#home'
+  get '/index', to: 'visitors#index', as: :index
   devise_for :users
   resources :users
 end
